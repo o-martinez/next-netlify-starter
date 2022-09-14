80b08b75411a5add7c9c5bd11a7cc95a6e0e4542
Omar <53627722+o-martinez@users.noreply.github.com>
2022-09-14 13:42:52 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,27 @@ export default function Home() {
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
+        <p>
+          <script>
+          var requestBody = ""; 
+
+var client=new XMLHttpRequest();
+client.open("get","https://digitalworkflow.service-now.com/api/now/table/x_snc_driver_fee_0_driver_feedback?sysparm_limit=1");
+
+client.setRequestHeader('Accept','application/json');
+client.setRequestHeader('Content-Type','application/json');
+
+//Eg. UserName="admin", Password="admin" for this code sample.
+client.setRequestHeader('Authorization', 'Basic '+btoa('admin'+':'+'1AppEngine!'));
+
+client.onreadystatechange = function() { 
+	if(this.readyState == this.DONE) {
+		document.getElementById("response").innerHTML=this.status + this.response; 
+	}
+}; 
+client.send(requestBody);
+          </script>
+        </p>
       </main>
 
       <Footer />
